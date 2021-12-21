a9bd80233186d856d3e43b4b0d15aaba47a07c76
rachan <rshetty@nttglobal.net>
2021-12-21 15:01:47 +0530

    study_drills - decalreing variables & calling later & printing the value

---
+++ ex4.py
@@ -0,0 +1,20 @@
+
+#decalring variables
+cars = 100
+people = 90
+space_in_car= 4
+drivers = 30
+cars_not_drivers = cars-drivers
+cars_with_drivers= drivers
+avg_people_per_car = people / cars_with_drivers
+
+
+#the eg used in book,there was typo error varaiable which was decalred as this "carpool_capacity" & while calling it was " car_pool_capacity "
+
+#printing all above results 
+print("There are",cars,"cars avaiable")
+print("There are", cars, "cars available.")
+print("there are total",people, "passengers.")
+print("how many passengers can seat in avg per car:",avg_people_per_car)
+print("total cars with driven:",cars_with_drivers)
+print("car not_driven:",cars_not_drivers)
