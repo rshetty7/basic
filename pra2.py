8f950c3282b96c0212eb47e2b34d17ef7e5d75a0
rachan <rshetty@nttglobal.net>
2022-01-14 14:33:06 +0530

    practice

---
+++ pra2.py
@@ -0,0 +1,9 @@
+
+
+
+s = input("Do you agree?\n")
+
+if s == "Y" or s == "y":
+    print("Agreed.")
+elif s == "N" or s == "n":
+    print("Not agreed.")
---
+++ prac1.py
@@ -0,0 +1,5 @@
+def cal(a, b):
+    return a+b, a-b
+add, sub = cal(40, 10)
+print(add)
+print(sub)
