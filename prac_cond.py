8b6909152b035390b7e53850994dbb9e3764803e
rachan <rshetty@nttglobal.net>
2022-01-17 11:13:06 +0530

    practice

---
+++ prac_cond.py
@@ -0,0 +1,15 @@
+
+
+x = input(int("x"))
+y = input(int("y"))
+
+value = input("Please enter an integer:\n")
+ 
+value = int(value)
+
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
